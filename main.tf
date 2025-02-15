5f7a2baf62fefd9172fc17b87af1243a274d861c
Prarthana Govindaraj <prarthanagovindaraj01@gmail.com>
2025-02-15 11:50:27 +0000

    addfile

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform{
+    required_providers {
+      google = {
+        source = "hashicorp/google"
+        version = "~>5.0"
+      }
+    }
+}
+
+provider "google"{
+    project = "corded-shift-339409"#actual project id
+    region = "us-west1"
+
+}
+
+resource "google_storage_bucket" "my_bucket" {
+    name = "my-terraform-bucket-222010"#should be globally unique
+    location = "us-west1"
+    versioning {
+      enabled = true
+    }
+  
+}
+
+output "bucket_name"{
+    value = google_storage_bucket.my_bucket.name
+}
